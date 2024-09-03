5d0f47e9a774ee4386f90a43d1b8fcc4b6ba5957
Torj <tornike.zoidze@gmail.com>
2024-09-03 16:18:31 +0400

    fixed adding music without author

--- src/music/music.repository.ts
+++ src/music/music.repository.ts
@@ -11,24 +11,27 @@ import { AuthorRepository } from 'src/author/author.repository';
 export class MusicRepository {
   constructor(@InjectRepository(MusicEntity)
   private MusicRepository: Repository<MusicEntity>,
-  private readonly authorRepo: AuthorRepository) { }
+    private readonly authorRepo: AuthorRepository) { }
   async create(createMusicDto: CreateMusicDto) {
-    const newMusic  = new MusicEntity
+    const newMusic = new MusicEntity
     const authorArr = []
     newMusic.name = createMusicDto.name
-    newMusic.duration  = createMusicDto.duration
-  
+    newMusic.duration = createMusicDto.duration
 
-    for(let i = 0; i < createMusicDto.authors.length; i++){
-      const author = await this.authorRepo.findOne(+createMusicDto.authors[i])
-      authorArr.push(author)
-    
+
+    if (createMusicDto.authors && createMusicDto.authors.length > 0) {
+      for (let i = 0; i < createMusicDto.authors.length; i++) {
+        const author = await this.authorRepo.findOne(+createMusicDto.authors[i])
+        authorArr.push(author)
+
+      }
     }
-     newMusic.authors =  authorArr
+    newMusic.authors = authorArr
 
     return await this.MusicRepository.save(newMusic)
-  
+
   }
+
 
   async findAll() {
     return await this.MusicRepository
@@ -48,47 +51,49 @@ export class MusicRepository {
   }
 
   async update(id: number, updateMusicDto: UpdateMusicDto) {
-    const musicToUpdate = await this.MusicRepository.findOneBy({ id })
-    
+    const musicToUpdate = await this.MusicRepository.findOneBy({ id });
+
     if (!musicToUpdate) {
-        throw new Error('Music entity not found');
+      throw new Error('Music entity not found');
     }
 
-    musicToUpdate.name = updateMusicDto.name || musicToUpdate.name
-    musicToUpdate.duration = updateMusicDto.duration || musicToUpdate.duration
-  
-    const authorArr = [];
-    for (let i = 0; i < updateMusicDto.authors.length; i++) {
+    musicToUpdate.name = updateMusicDto.name || musicToUpdate.name;
+    musicToUpdate.duration = updateMusicDto.duration || musicToUpdate.duration;
+
+    if (updateMusicDto.authors) {
+      const authorArr = [];
+      for (let i = 0; i < updateMusicDto.authors.length; i++) {
         const author = await this.authorRepo.findOne(+updateMusicDto.authors[i]);
         if (author) {
-            authorArr.push(author);
+          authorArr.push(author);
         }
+      }
+      musicToUpdate.authors = authorArr;
     }
-    
-    musicToUpdate.authors = authorArr;
 
     return await this.MusicRepository.save(musicToUpdate);
-}
+  }
+
 
   async remove(id: number) {
     await this.MusicRepository
       .createQueryBuilder('music')
       .softDelete()
-      .from(MusicEntity)
-      .where('music.id = :id', { id })
-      .execute()
+      .where('id = :id', { id })
+      .execute();
 
     return this.MusicRepository
       .createQueryBuilder('music')
       .withDeleted()
-      .where('music.id = :id', { id })
-      .execute()
+      .where('id = :id', { id })
+      .getOne();
   }
 
-  async search(query: string){
+
+  async search(query: string) {
     return this.MusicRepository
-    .createQueryBuilder('music')
-    .where('music.name LIKE :query', {query: `%${query}%`})
-    .getMany()
+      .createQueryBuilder('music')
+      .where('music.name LIKE :query', { query: `%${query}%` })
+      .getMany()
   }
 }
